30b4a2256e24e5ecd848cbd5e0be56215bffdc8b
Yuning Lin <pandaning9112@gmail.com>
2015-10-26 23:31:55 +0800

    Build random tree for test
    I building random binary tree in these two files to test the execution
    time, and the freeNode function is for free every node malloced before
    so that memory and program can execute good.
    
    Revise the bug of execution time, and fix some coding style.

--- q3/iterative.c
+++ q3/iterative.c
@@ -16,17 +16,31 @@ static double diff_in_second(struct timespec t1, struct timespec t2)
     return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
 }
 
-struct TreeNode {
+struct TreeNode 
+{
     int val;
     struct TreeNode *left;
     struct TreeNode *right;
 };
 
+void freeNode(struct TreeNode *ptr)
+{
+    if(ptr -> left)
+    {
+        freeNode(ptr -> left);
+    }
+    if(ptr -> right)
+    {
+        freeNode(ptr -> right);
+    }
+    free(ptr);
+}
 void flatten(struct TreeNode* root)
 {
     if(root==NULL) return ;
     struct TreeNode *ptr, *tmp;
-    while(root) {
+    while(root) 
+    {
         tmp=root->right;
         root->right=root->left;
         root->left=NULL;
@@ -41,7 +55,8 @@ void flatten(struct TreeNode* root)
 void CreateNode(struct TreeNode *tmp,char *route,int num)
 {
     int i=0;
-    for(; i<strlen(route); i++) {
+    for(; i<strlen(route); i++) 
+    {
         if(route[i]=='L') {
             if(tmp->left==NULL) {
                 tmp->left=malloc(sizeof(struct TreeNode));
@@ -51,7 +66,8 @@ void CreateNode(struct TreeNode *tmp,char *route,int num)
             } else {
                 tmp=tmp->left;
             }
-        } else if(route[i]=='R') {
+        } 
+        else if(route[i]=='R') {
             if(tmp->right==NULL) {
                 tmp->right=malloc(sizeof(struct TreeNode));
                 tmp=tmp->right;
@@ -73,6 +89,41 @@ void output_node(struct TreeNode* tmp)
         printf("%d\n",tmp->val);
     }
 }
+void BuildRandomTree(struct TreeNode *root,int nodeNum)
+{
+    srand(time(0));
+    char branch[][2]={"L","R"};
+    char store[nodeNum][nodeNum];
+    int mark[nodeNum][2];
+    int Num[nodeNum];
+    memset(store,0,sizeof(store));
+    memset(mark,0,sizeof(mark));
+    memset(Num,0,sizeof(Num));
+    Num[0] = 1;
+    int curNum = 1;
+    for(curNum;curNum < nodeNum ;curNum++)
+    {
+        int num;
+        do
+        {
+            num=rand()%nodeNum;
+        }while(Num[num]);
+       Num[num]=1;
+        int LR=rand()%2;
+        int randNode;
+        do
+        {
+            randNode=rand()%curNum;
+        }while(mark[randNode][LR]);
+        mark[randNode][LR]=1;
+        char newNode[nodeNum];
+        memset(newNode,0,sizeof(newNode));
+        strcpy(newNode,store[randNode]);
+        strcat(newNode,branch[LR]);
+        CreateNode(root,newNode,num);
+        strcpy(store[curNum],newNode);
+    }
+}
 int main()
 {
     FILE *file = fopen("q3_iterative.txt", "a");
@@ -82,24 +133,15 @@ int main()
     a = malloc(sizeof(struct TreeNode));
     a->right=NULL;
     a->left=NULL;
-    CreateNode(a,"",5);
-    CreateNode(a,"L",2);
-    CreateNode(a,"LL",3);
-    CreateNode(a,"LR",6);
-    CreateNode(a,"R",1);
-    CreateNode(a,"RR",4);
+    CreateNode(a,"",0);
+    int nodeNum = 10;
+    BuildRandomTree(a,nodeNum);
     clock_gettime(CLOCK_REALTIME, &start);
     flatten(a);
     clock_gettime(CLOCK_REALTIME, &end);
-    output_node(a);
+    cpu_time1 = diff_in_second(start, end);
+    freeNode(a);
     printf("execution time: %lf sec\n", cpu_time1);
     fprintf(file, "%f\n", cpu_time1);
     return 0;
 }
-/*
-  *              5
-  *             / \
-  *            2   1
-  *           / \    \
-  *          3   6   4
-  */
--- q3/recursive.c
+++ q3/recursive.c
@@ -31,6 +31,18 @@ void init()
     ptr->right=NULL;
     ptr->left=NULL;
 }
+void freeNode(struct TreeNode *ptr)
+{
+	if(ptr -> left)
+	{
+		freeNode(ptr -> left);
+	}
+	if(ptr -> right)
+	{
+		freeNode(ptr -> right);
+	}
+	free(ptr);
+}
 void preorder(struct TreeNode *root)
 {
     if(root==NULL) return ;
@@ -67,7 +79,8 @@ void CreateNode(struct TreeNode *tmp,char *route,int num)
             } else {
                 tmp=tmp->left;
             }
-        } else if(route[i]=='R') {
+        } 
+        else if(route[i]=='R') {
             if(tmp->right==NULL) {
                 tmp->right=malloc(sizeof(struct TreeNode));
                 tmp=tmp->right;
@@ -80,13 +93,48 @@ void CreateNode(struct TreeNode *tmp,char *route,int num)
     }
     tmp->val=num;
 }
-void output_node(struct TreeNode* tmp)
+void output_node(struct TreeNode* ptr)
 {
-    if(tmp==NULL)return;
-    else printf("%d\n",tmp->val);
-    while(tmp->right) { //printf all
-        tmp=tmp->right;
-        printf("%d\n",tmp->val);
+    if(ptr==NULL)return;
+    else printf("%d\n",ptr->val);
+    while(ptr->right) { //printf all
+        ptr=ptr->right;
+        printf("%d\n",ptr->val);
+    }
+}
+void BuildRandomTree(struct TreeNode *root,int nodeNum)
+{
+    srand(time(0));
+    char branch[][2]={"L","R"};
+    char store[nodeNum][nodeNum];
+    int mark[nodeNum][2];
+    int Num[nodeNum];
+    memset(store,0,sizeof(store));
+    memset(mark,0,sizeof(mark));
+    memset(Num,0,sizeof(Num));
+    Num[0] = 1;
+    int curNum = 1;
+    for(curNum;curNum < nodeNum ;curNum++)
+    {
+        int num;
+        do
+        {
+            num=rand()%nodeNum;
+        }while(Num[num]);
+       Num[num]=1;
+        int LR=rand()%2;
+        int randNode;
+        do
+        {
+            randNode=rand()%curNum;
+        }while(mark[randNode][LR]);
+        mark[randNode][LR]=1;
+        char newNode[nodeNum];
+        memset(newNode,0,sizeof(newNode));
+        strcpy(newNode,store[randNode]);
+        strcat(newNode,branch[LR]);
+        CreateNode(root,newNode,num);
+        strcpy(store[curNum],newNode);
     }
 }
 int main()
@@ -98,24 +146,15 @@ int main()
     a = malloc(sizeof(struct TreeNode));
     a->right=NULL;
     a->left=NULL;
-    CreateNode(a,"",5);
-    CreateNode(a,"L",2);
-    CreateNode(a,"LL",3);
-    CreateNode(a,"LR",6);
-    CreateNode(a,"R",1);
-    CreateNode(a,"RR",4);
+    CreateNode(a,"",0);
+    int nodeNum = 10;
+    BuildRandomTree(a,nodeNum);
     clock_gettime(CLOCK_REALTIME, &start);
     flatten(a);
     clock_gettime(CLOCK_REALTIME, &end);
-    output_node(a);
+    cpu_time1 = diff_in_second(start, end);
+    freeNode(a);
     printf("execution time: %lf sec\n", cpu_time1);
     fprintf(file, "%f\n", cpu_time1);
     return 0;
 }
-/*
-  *              5
-  *             / \
-  *            2   1
-  *           / \   \
-  *          3   6   4
-  */
