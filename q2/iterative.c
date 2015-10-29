df98986e94dd7f85f13b02575a1cf4cee7d65e44
Yuning Lin <pandaning9112@gmail.com>
2015-10-29 16:26:59 +0800

    Revise test method and implement sched_setscheduler
    I use rand() to generate random characters for the smallese_character function,
    and test this function from 'a' to 'z' in order to know whether the answer is
    correct.
    
    Use sched_setscheduler to make this program is execute for the top priority,
    and get the execution time more objective.
    
    Use astyle.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <assert.h>
+#include <sched.h>
 
 static double diff_in_second(struct timespec t1, struct timespec t2)
 {
@@ -21,26 +21,41 @@ char smallest_character(char str[],char fc)
 {
     int n=strlen(str);
     int i;
-    for(i=0; i<n; i++) {
+    for(i=0; i<n; i++)
         if(str[i]>fc)return str[i];
-    }
     return str[0];
 }
+
 int main()
 {
     FILE *file = fopen("q2_iterative.txt", "a");
     double cpu_time1;
     struct timespec start, end;
+    srand(time(NULL));
+    int random_len = (rand() % 10) + 1;
     char i;
-    char str[]= {'a','d','g','j','m','p','s','v','z'}; //input test
+    static const char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+    char str[random_len];
+    struct sched_param param;
+    int maxpri;
+    maxpri = sched_get_priority_max(SCHED_FIFO);//max=99, min=1
+    if(maxpri == -1) {
+        perror("sched_get_priority_max() failed");
+        exit(1);
+    }
+    param.sched_priority = maxpri;
+    if (sched_setscheduler(getpid(), SCHED_FIFO, &param) == -1) {
+        perror("sched_setscheduler() failed");
+        exit(1);
+    }
+    memset(str, 0, sizeof(str));
+    for(i=0; i<random_len; i++) str[i] = alpha[rand() % (strlen(alpha) - 1)];
     for(i=97; i<123; i++) { //test from a to z
-        printf("Test: [%c]\n",i);
         clock_gettime(CLOCK_REALTIME, &start);
-        smallest_character(str,i);
+        assert(smallest_character(str,i));
         clock_gettime(CLOCK_REALTIME, &end);
-        printf("Output: %c\n",smallest_character(str,i));
+        printf("Output[%c]: %c\n",i, smallest_character(str,i));
         cpu_time1 = diff_in_second(start, end);
-        printf("execution time: %lf sec\n", cpu_time1);
         fprintf(file, "%f\n", cpu_time1);
     }
     return 0;
